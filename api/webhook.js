e66e293148617f33fede52ad3d23a4e757ff3b36
ogarridojimenez <ogarridojimenez@gmail.com>
2026-05-07 15:03:02 -0400

    fix api

--- api/webhook.js
+++ api/webhook.js
@@ -1,20 +1,8 @@
 module.exports = async (req, res) => {
 
-  // GET
-  if (req.method === "GET") {
-    return res.status(200).send("Webhook funcionando 🚀");
-  }
-
-  // POST TEST
-  if (req.method === "POST") {
-
-    return res.status(200).json({
-      success: true,
-      body: req.body
-    });
-
-  }
-
-  return res.status(405).send("Method not allowed");
+  return res.status(200).json({
+    success: true,
+    method: req.method
+  });
 
 };
