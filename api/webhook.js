ff147ca4e703527e467892b67c0f66c8ec441d41
ogarridojimenez <ogarridojimenez@gmail.com>
2026-05-08 11:00:42 -0400

    fix: logs payload.text

--- api/webhook.js
+++ api/webhook.js
@@ -101,7 +101,7 @@ module.exports = async (req, res) => {
 
     console.error( error);
 
-    return res.status( 500 ).json({
+    return res.status( 500  ).json({
       success: false,
       error: error.message,
     });
